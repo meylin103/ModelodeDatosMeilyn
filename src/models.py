b8004c28f32ba07a883497259a33b292e5231bdc
Meilyn F <92810092+meylin103@users.noreply.github.com>
2026-05-06 22:50:08 +0000

    modelo planetas

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_columnn(String(120),(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
@@ -16,4 +16,32 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=false, nullable=False)
+    climate: Mapped[str] = mapped_columnn(String(120),(nullable=False)
+    diameter: Mapped[int] = mapped_columnn(Integer,nullable=False)
+   
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "diameter": self.diameter,
+            
+        }
+#people
+#favorites
+class favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+   
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            
         }
